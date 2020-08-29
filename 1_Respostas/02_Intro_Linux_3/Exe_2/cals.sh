f7bf90c22a6914c33a70d48941ab3513d96a0a63
BismarkCT <bismarkcotrim@hotmail.com>
2020-08-29 02:13:48 -0300

    Fix cals.sh

--- 1_Respostas/02_Intro_Linux_3/Exe_2/cals.sh
+++ 1_Respostas/02_Intro_Linux_3/Exe_2/cals.sh
@@ -1,22 +1,14 @@
 #!/bin/bash
 #Exemplo 2 do 02_Intro_Linux_3
-for i in $(seq $#) 
+a=1
+for i in $@
 do
- if [ $((i%2)) == 1 ]; then
-  case $i in 
-   '1')
-       cal $1 $2
-       ;;
-   '3')
-       cal $3 $4
-       ;;
-   '5')
-       cal $5 $6
-       ;;
-   '7')
-       cal $7 $8
-       ;;
-  esac    
- fi
+if [ $a == 1 ]; then
+    b=$i
+    a=0
+else
+    c=$i
+    a=1
+    cal $b $c 
+fi
 done
-
